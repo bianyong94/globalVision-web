7481413007ca246b6a59844a360b25a8daad0395
18874034861 <649318836@qq.com>
2025-12-23 17:19:31 +0800

    优化

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { fetchHomeData } from "../services/api"
-import { HomeData, Video } from "../types"
+import { HomeData } from "../types"
 import VideoCard from "../components/VideoCard"
 import {
   Loader2,
